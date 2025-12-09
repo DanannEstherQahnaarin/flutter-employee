41efc6d8ee07cf904db0d3c47ced7803a825e827
kmg <deepestdark@gmail.com>
2025-12-09 17:47:07 +0900

    list add screen update

--- lib/providers/emp_provider.dart
+++ lib/providers/emp_provider.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_employee/model/employee.dart';
 
 class EmployeeProvider extends ChangeNotifier {
-  // growable 리스트를 명시적으로 사용해 add/remove 시 예외를 방지
-  final List<Employee> _empList = List<Employee>.empty(growable: true);
+  final List<Employee> _empList = List.empty(growable: true);
 
   List<Employee> get empList => _empList;
 
-  void addEmp(Employee emp) {
-    _empList.add(emp);
-
+  void addEmp(Employee e) {
+    _empList.add(e);
     notifyListeners();
   }
 
